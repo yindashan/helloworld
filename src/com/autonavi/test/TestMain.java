e33a26110660cf1ecfc8ea90ee49647b8676e6da
dashan.yin <dashan.yin@YinDaShan-E5400.autonavi.com>
2013-02-04 15:22:35 +0800

    修改编码由GBK改成UTF-8

--- src/com/autonavi/test/TestMain.java
+++ src/com/autonavi/test/TestMain.java
@@ -3,11 +3,11 @@ package com.autonavi.test;
 public class TestMain {
 	public static void main(String [] args) {
 		System.out.println("Hello World!");
-		// ��������
+		// 初始化
 		System.out.println("回家过年");
 		System.out.println("远程修改同步到本地！");
-		// �ٴ��޸�
-		System.out.println("�����޸ģ�");
+		// 本地修改
+		System.out.println("本地修改");
 		// 服务器远端修改
 		System.out.println("服务器远端修改！");
 		
