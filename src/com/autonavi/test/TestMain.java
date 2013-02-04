0ff5fb543c3509a18516f16771cc52bda8c786ab
dashan.yin <dashan.yin@YinDaShan-E5400.autonavi.com>
2013-02-04 14:42:17 +0800

    添加一行代码

--- src/com/autonavi/test/TestMain.java
+++ src/com/autonavi/test/TestMain.java
@@ -3,6 +3,8 @@ package com.autonavi.test;
 public class TestMain {
 	public static void main(String [] args) {
 		System.out.println("Hello World!");
+		// 修改操作
+		System.out.println("回家过年！");
 	}
 
 }
